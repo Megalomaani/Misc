718608535c67f388302f2c50b078ce9247907f9b
Lari <lari.karvinen@gmail.com>
2018-12-31 02:13:43 +0200

    bot dp feature fix

--- hellobotti.py
+++ hellobotti.py
@@ -11,7 +11,7 @@ def handle(msg):
     if command == '/moro':
         bot.sendMessage(chat_id, "No haista sina huora vittu!")
     elif command == '/dickpick':
-            bot.send_photo(chat_id=chat_id, photo=open('dp.png', 'rb'))  
+            bot.sendPhoto(chat_id=chat_id, photo=open('dp.png', 'rb'))  
     else:
         bot.sendMessage(chat_id, "Haista sina huora kyrpa!")
 
